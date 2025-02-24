c06d7e0e5b8848f0dc6b1949404dd5e99cb38a39
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2025-02-24 05:21:31 -0300

    actualizaciónd de login, validación

--- Backend/Domain/UseCases/LoginUser.cs
+++ Backend/Domain/UseCases/LoginUser.cs
@@ -5,28 +5,26 @@ using BCrypt.Net;
 
 public class LoginUser
 {
-    // Dependencia inyectada de la interfaz IUserRepository
     private readonly IUserRepository _userRepository;
 
-    // Constructor que recibe una instancia de IUserRepository para realizar operaciones en la base de datos
     public LoginUser(IUserRepository userRepository)
     {
         _userRepository = userRepository;
     }
 
-    // Método para autenticar un usuario con su legajo y contraseña
-    public async Task<User?> AuthenticateAsync(string legajo, string password)
+    public async Task<(bool Success, string Error, User? User)> AuthenticateAsync(string legajo, string password)
     {
-        // Busca el usuario en el repositorio utilizando el legajo
         var user = await _userRepository.GetByLegajoAsync(legajo);
-
-        // Si el usuario no existe o la contraseña es incorrecta, retorna null
-        if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.password_hash))
+        if (user == null)
         {
-            return null;
+            return (false, "LEGAJO INCORRECTO", null);
         }
 
-        // Si la autenticación es correcta, retorna el usuario
-        return user;
+        if (!BCrypt.Net.BCrypt.Verify(password, user.password_hash))
+        {
+            return (false, "CONTRASEÑA INCORRECTA", null);
+        }
+
+        return (true, "", user);
     }
 }
