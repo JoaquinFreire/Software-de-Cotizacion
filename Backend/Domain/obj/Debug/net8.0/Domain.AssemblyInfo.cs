f48b3307459f701abc39c7bff4789303d8167d37
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2025-02-26 06:56:16 -0300

    Creación de links y navbar mejorado para páginas.

--- Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca3a3d4e2d39de9bb2aeb648e2be477904f0552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bab7dd86e1c40b9745526091af854d84dfbf7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
