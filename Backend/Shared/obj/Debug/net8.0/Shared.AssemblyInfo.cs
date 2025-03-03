dfce0c8b4a8a9af4794ae2a996e353b5fb6f31c8
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2025-03-02 23:26:53 -0300

    Bd copy

--- Backend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Backend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0a91d18f723dcadc5424b6acc563bcb6329b3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d23859e32fb0b605a825d2dd869918907fb0255")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
