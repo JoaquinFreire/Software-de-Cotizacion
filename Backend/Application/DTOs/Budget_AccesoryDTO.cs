87d179bee2d48ae32dc172e9f1f754d2d4ab865d
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2025-03-13 14:11:00 -0300

    DTOs agregados modo simple, sin error

---
+++ Backend/Application/DTOs/Budget_AccesoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class Budget_AccesoryDTO
+    {
+        public int? Quantity { get; set; }
+        public string? Name { get; set; }
+        public MaterialType? Type { get; set; }
+    }
+}
