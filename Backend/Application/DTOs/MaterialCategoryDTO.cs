9fe22b10db62a2ca4e713e99ffcc0af0c16630e9
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2025-03-13 14:11:00 -0300

    DTOs agregados modo simple, sin error

---
+++ Backend/Application/DTOs/MaterialCategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class MaterialCategoryDTO
+    {
+        public string? name { get; set; }
+        public string? unit { get; set; }
+    }
+}
---
+++ Backend/Application/DTOs/MaterialDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class MaterialDTO
+    {
+        public string? name { get; set; }
+        public MaterialTypeDTO? type { get; set; }
+        public double? price { get; set; }
+    }
+}
