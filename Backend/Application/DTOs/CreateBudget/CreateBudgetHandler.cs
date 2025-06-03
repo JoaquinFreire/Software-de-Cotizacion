314d10c17207bd5989ef0c0993d4be51fab6db75
LeoMorales314 <leonardo.morales@mi.unc.edu.ar>
2025-06-02 21:00:23 -0300

    BudgetCalculator estructura

--- Backend/Application/DTOs/CreateBudget/CreateBudgetHandler.cs
+++ Backend/Application/DTOs/CreateBudget/CreateBudgetHandler.cs
@@ -22,6 +22,12 @@ namespace Application.DTOs.CreateBudget
         {
             var budget = _mapper.Map<Budget>(request.BudgetDTO);
 
+            var idGenerator = new BudgetIdGenerator();
+            budget.budgetId = idGenerator.GenerateBudgetId(
+                request.BudgetDTO.customer.name,
+                request.BudgetDTO.customer.lastname);
+
+
             await _budgetServices.CreateBudgetAsync(budget);
             return budget.id;
         }
