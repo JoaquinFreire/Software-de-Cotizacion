26101feb70057cf3056ca5d5d7fd2ca2296459d0
LeoMorales314 <leonardo.morales@mi.unc.edu.ar>
2025-07-21 22:01:33 -0300

    Implementación de complementos a Budget

---
+++ Backend/Domain/Entities/ComplementDoor.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ComplementDoor // Puerta
+    {
+        [BsonIgnore]
+        public int id { get; set; }
+        public string? name { get; set; }
+        public double price { get; set; }
+        public double width { get; set; } // Ancho de la puerta
+        public double height { get; set; } // Alto de la puerta
+        public string? Material { get; set; } // Material de la puerta
+        public List<Accesory> Accesory { get; set; } = new List<Accesory>();
+    }
+}
