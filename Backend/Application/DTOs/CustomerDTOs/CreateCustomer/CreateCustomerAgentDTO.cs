71c39a1ee1812bdaba4518aeeed5e54f90f8f40f
LeoMorales314 <leonardo.morales@mi.unc.edu.ar>
2025-08-09 16:20:08 -0300

    Parte 1 de actualización Mediatr

---
+++ Backend/Application/DTOs/CustomerDTOs/CreateCustomer/CreateCustomerAgentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.CustomerDTOs.CreateCustomer
+{
+    public class CreateCustomerAgentDTO
+    {
+        public required string name { get; set; }
+        public required string lastname { get; set; }
+        public required string tel { get; set; }
+        public required string mail { get; set; }
+    }
+}
---
+++ Backend/Application/DTOs/CustomerDTOs/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.DTOs.CustomerDTOs.CreateCustomer
+{
+    public class CreateCustomerCommand : IRequest<string>
+    {
+        public required CreateCustomerDTO createCustomerDTO { get; set; }
+    }
+}
