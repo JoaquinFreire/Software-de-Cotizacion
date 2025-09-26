8be306fd25df1cf6b2c946ad80a89ee19bf5ed9f
LeoMorales314 <leonardo.morales@mi.unc.edu.ar>
2025-09-26 00:10:04 -0300

    Reporte Linea de tiempo de cotizaciones parte 1

---
+++ Backend/Presentation/Controllers/TimeLineBudgetReportController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/TimeLineBudgetReport")]
+    public class TimeLineBudgetReportController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TimeLineBudgetReportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{budgetId}")]
+        public async Task<IActionResult> GetTimeLineBudgetReport(string budgetId)
+        {
+            try
+            {
+                var query = new Application.DTOs.BudgetDTOs.TimeLineBudgetReport.TimeLineBudgetReportQuery(budgetId);
+                var report = await _mediator.Send(query);
+                return Ok(report);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex) as needed
+                return StatusCode(500, "Ocurrió un error interno en el servidor.");
+            }
+        }
+    }
+}
