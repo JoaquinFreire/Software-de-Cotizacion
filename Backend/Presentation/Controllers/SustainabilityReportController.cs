96c1bea9b3c098e786f0f03dd902b4e42d74cfe1
LeoMorales314 <leonardo.morales@mi.unc.edu.ar>
2025-10-29 23:24:21 -0300

    Reporte de sostenibilidad economica versión 1

---
+++ Backend/Presentation/Controllers/SustainabilityReportController.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs.SustainabilityReportDTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/SustainabilityReport")]
+    public class SustainabilityReportController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SustainabilityReportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("metrics")]
+        public async Task<ActionResult<SustainabilityMetricsDTO>> GetSustainabilityMetrics(
+            [FromQuery] DateTime? fromDate,
+            [FromQuery] DateTime? toDate,
+            [FromQuery] string? timeRange = "12M")
+        {
+            var query = new GetSustainabilityMetricsQuery
+            {
+                FromDate = fromDate,
+                ToDate = toDate,
+                TimeRange = timeRange
+            };
+
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
